917d5baaf217ff14f643490d4536d80e28f06aa2
cclvi256 <51155451+cclvi256@users.noreply.github.com>
2023-10-14 02:44:56 +0800

    Completed the design of structures; finished the structures CharInfo, TreeElem, and TreeNode

--- Huffman.cpp
+++ Huffman.cpp
@@ -1,20 +1,255 @@
-﻿// Huffman.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
 
-#include <iostream>
+#define CAPACITY 95
 
-int main()
+struct CharInfo
 {
-    std::cout << "Hello World!\n";
+	char ch;
+	char huffmanCodeBits;
+	int count;
+	int huffmanCode;
+
+	CharInfo();
+	CharInfo(char ch);
+	CharInfo(char ch, int count);
+	void AttachCode(int huffmanCode, char huffmanCodeBits);
+};
+
+struct TreeElem
+{
+	char ch;
+	bool isLeaf;
+	int totalCount;
+
+	TreeElem();
+};
+
+struct TreeNode
+{
+	TreeElem value;
+	TreeNode* l;
+	TreeNode* r;
+
+	TreeNode();
+	TreeNode(char ch, CharInfo* dictionary);
+	TreeNode(TreeNode& sub1, TreeNode& sub2);
+
+	void DictionaryFixing(CharInfo* dictionary);
+	void DictionaryFixing(CharInfo* dictionary, int depth, int position);
+
+	const bool operator<(const TreeNode& operand2) const;
+};
+
+struct IBuffer
+{
+	char buffer;
+	char remaining;
+
+	IBuffer(std::string filePath, int totalLength);
+
+	bool GetBit();
+	bool PeekBit();
+};
+
+struct OBuffer
+{
+	char buffer;
+	char remaining;
+
+	OBuffer(std::string filePath);
+
+	void PutBit(bool bit);
+	void PutBit(int bits, int num);
+	void FixEnd();
+};
+
+struct Heap
+{
+	TreeNode* root;
+	int capacity;
+	int currentIndex;
+
+	Heap();
+	Heap(int capacity);
+
+	void Push(TreeNode& value);
+	void Pop();
+};
+
+void CountOccurrence(std::string filePath, CharInfo* countArray);
+void Compress(std::string sourceFilePath, std::string outputFilePath);
+void Decompress(std::string compressedFilePath);
+void Decompress(std::string compressedFilePath, std::string outputFilePath);
+
+/*********************************************************************************/
+/*********************************************************************************/
+/*********************************************************************************/
+
+int main(int argc, char** argv)
+{
+	return 0;
 }
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
+/*********************************************************************************/
+/*********************************************************************************/
+/*********************************************************************************/
 
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
+CharInfo::CharInfo() :
+	ch(0),
+	huffmanCode(0),
+	huffmanCodeBits(0),
+	count(0)
+{
+}
+
+CharInfo::CharInfo(char ch) :
+	ch(ch),
+	huffmanCode(0),
+	huffmanCodeBits(0),
+	count(0)
+{
+}
+
+CharInfo::CharInfo(char ch, int count) :
+	ch(ch),
+	huffmanCode(0),
+	huffmanCodeBits(0),
+	count(count)
+{
+}
+
+void CharInfo::AttachCode(int huffmanCode, char huffmanCodeBits)
+{
+}
+
+/*********************************************************************************/
+
+TreeElem::TreeElem() :
+	ch(0),
+	isLeaf(true),
+	totalCount(0)
+{
+}
+
+TreeNode::TreeNode() :
+	value(TreeElem()),
+	l(nullptr),
+	r(nullptr)
+{
+}
+
+TreeNode::TreeNode(char ch, CharInfo* dictionary) :
+	value(TreeElem()),
+	l(nullptr),
+	r(nullptr)
+{
+	value.ch = ch;
+	value.isLeaf = true;
+	value.totalCount = dictionary[ch - 32].count;
+}
+
+TreeNode::TreeNode(TreeNode& sub1, TreeNode& sub2) :
+	value(TreeElem()),
+	l(&sub1),
+	r(&sub2)
+{
+	value.ch = 0;
+	value.isLeaf = false;
+	value.totalCount = sub1.value.totalCount + sub2.value.totalCount;
+}
+
+void TreeNode::DictionaryFixing(CharInfo* dictionary)
+{
+	DictionaryFixing(dictionary, 0, 0);
+}
+
+void TreeNode::DictionaryFixing(CharInfo* dictionary, int depth, int position)
+{
+	if (value.isLeaf)
+	{
+		dictionary[value.ch - 32].AttachCode(position, depth);
+	}
+	else
+	{
+		(*l).DictionaryFixing(dictionary, depth + 1, position * 2);
+		(*r).DictionaryFixing(dictionary, depth + 1, position * 2 + 1);
+	}
+}
+
+const bool TreeNode::operator<(const TreeNode& operand2) const
+{
+	return value.totalCount < operand2.value.totalCount;
+}
+
+/*********************************************************************************/
+
+IBuffer::IBuffer(std::string filePath, int totalLength)
+{
+}
+
+bool IBuffer::GetBit()
+{
+	return false;
+}
+
+bool IBuffer::PeekBit()
+{
+	return false;
+}
+
+/*********************************************************************************/
+
+OBuffer::OBuffer(std::string filePath)
+{
+}
+
+void OBuffer::PutBit(bool bit)
+{
+}
+
+void OBuffer::PutBit(int bits, int num)
+{
+}
+
+void OBuffer::FixEnd()
+{
+}
+
+/*********************************************************************************/
+
+Heap::Heap()
+{
+}
+
+Heap::Heap(int capacity)
+{
+}
+
+void Heap::Push(TreeNode& value)
+{
+}
+
+void Heap::Pop()
+{
+}
+
+/*********************************************************************************/
+
+void CountOccurrence(std::string filePath, CharInfo* countArray)
+{
+}
+
+void Compress(std::string sourceFilePath, std::string outputFilePath)
+{
+}
+
+void Decompress(std::string compressedFilePath)
+{
+}
+
+void Decompress(std::string compressedFilePath, std::string outputFilePath)
+{
+}
